ea587dda711c954c13e002d149ed0227e3ccab5f
dana <aa>
2026-03-02 20:15:10 +0000

    sss

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- * _isupper()
-	check if the letter is upper character by returning it one
-*
-Return: one if its uppercase and 0 otherwise
-*/
+ * _isupper - checks for uppercase character
+ * @c: the character to check
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 int _isupper(char c)
 {
-	if(c >= 'A' && c <= 'Z'){
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	}
-	else{
+	else
 		return (0);
-	}
 }
