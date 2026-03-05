34fa540e023a449896f05eb3fe0fad1aab947d4b
dd <aa>
2026-03-05 21:17:24 +0000

    aaa

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - uuuuu
+ * @s: pppppp
+ *
+ * Return: int llll
+ */
+
+int _strlen(char *s)
+{
+	int d = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		d++;
+	}
+
+	return (d);
+}
