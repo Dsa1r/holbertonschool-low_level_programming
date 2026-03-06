ad132538c92dbcc9e87b9b1c704410f7da522d9d
aa <dana>
2026-03-06 20:31:34 +0000

    aa

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - printdd
+ * @s: the pointer of the string
+ *
+ * Return: void
+ */
+
+
+
+void print_rev(char *s)
+{
+	int _strlen(char *s);
+	int d =  _strlen(s);
+
+	while (d > 0)
+	{
+		_putchar(s[d]);
+		d--;
+	}
+
+	_putchar('\n');
+}
