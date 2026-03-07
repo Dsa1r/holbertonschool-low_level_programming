2724155154097a3708763e7a2761f5dc2741345d
aaa <aaa>
2026-03-07 16:04:52 +0000

    aaa

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string in place
+ * @s: Pointer to the string to reverse
+ *
+ * Return: Nothing (void)
+ */
+
+
+void rev_string(char *s)
+{
+	int length = 0;
+	int start, end;
+	char temp;
+	
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	start = 0;
+	end = length - 1;
+
+	while (start < end)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+		
+		start++;
+		end--;
+	}
+}
