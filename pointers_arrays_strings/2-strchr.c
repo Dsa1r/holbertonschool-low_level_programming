c85772bfb25e4521809222940379a9cc6512db92
wwew <aaa>
2026-03-09 20:49:13 +0000

    sss

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,20 @@
 #include "main.h"
 #include <stddef.h>
 
+/**
+ * _strchr - llllll
+ * @s: 9000
+ * @c: dkdk
+ *
+ * Return: dddd
+ */
+
 
 char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (s[i] != '\0' 77 c != '\0')
 	{
 		if (s[i] == c)
 			return (&s[i]);
