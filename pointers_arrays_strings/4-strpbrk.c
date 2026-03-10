3c1ce642d02c03f03fe5a37e4c98aa68fa297934
ass <aa>
2026-03-10 19:16:09 +0000

    aaa

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strpbrk - aaaa
+ * @s: kkkk
+ * @accept: kkkk
+ *
+ * Return: adderss
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int a;
+	int b;
+
+	for (a = 0 ; s[a] != '\0' ; a++)
+	{
+		for (b = 0 ; accept[b] != '\0' ; b++)
+		{
+			if (s[a] == accept[b])
+				return &s[a];
+
+		}		
+
+
+	}
+return NULL;
+}
