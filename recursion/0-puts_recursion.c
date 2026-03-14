16a5c7c5ba386fccb1ca1d12310483dd58c8e5e0
sss <sss>
2026-03-14 16:45:04 +0000

    sss

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string followed by a new line using recursion
+ * @s: The string to print
+ *
+ * Return: Nothing (void)
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')  /* Base case: end of string reached */
+	{
+		_putchar('\n');  /* Print new line */
+		return;
+	}
+
+	_putchar(*s);  /* Print current character */
+	_puts_recursion(s + 1);  /* Recursive call with next character */
+}
