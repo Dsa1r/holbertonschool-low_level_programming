20f479ca2166acd7aa3a7c03b6019f0baa82d4e8
dssds <dddd>
2026-03-29 15:22:26 +0000

    sss

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -13,16 +13,17 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *array = malloc(size * sizeof(char));
 
 	if (size == 0)
 		return (NULL);
 
-	for (i = 0; i < size ; i++)
-		array[i] = c;
+	char *array = malloc(size * sizeof(char));
 
 	if (array == NULL)
 		return (NULL);
+
+	for (i = 0; i < size ; i++)
+		array[i] = c;
 
 	return (array);
 }
