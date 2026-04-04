2317c3b247c3db7a42f078354dd6befdc827c199
dkdkdkkd <ldld>
2026-04-04 15:47:30 +0000

    ddd

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,6 +2,33 @@
 #include "dog.h"
 
 /**
+ * _strdup_custom - duplicates a string without using strcpy/strlen
+ * @str: string to duplicate
+ *
+ * Return: pointer to new string, NULL on failure
+ */
+char *_strdup_custom(char *str)
+{
+	int i, len = 0;
+	char *copy;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
+
+	copy = malloc(len + 1);
+	if (copy == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+		copy[i] = str[i];
+
+	return (copy);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: dog's name
  * @age: dog's age
@@ -11,47 +38,24 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i, len_name = 0, len_owner = 0;
 	dog_t *d;
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
 
-	if (name != NULL)
-	{
-		for (len_name = 0; name[len_name]; len_name++)
-			;
-		d->name = malloc(len_name + 1);
-		if (d->name == NULL)
-		{
-			free(d);
-			return (NULL);
-		}
-		for (i = 0; i <= len_name; i++)
-			d->name[i] = name[i];
-	}
-	else
-		d->name = NULL;
-
-	if (owner != NULL)
-	{
-		for (len_owner = 0; owner[len_owner]; len_owner++)
-			;
-		d->owner = malloc(len_owner + 1);
-		if (d->owner == NULL)
-		{
-			free(d->name);
-			free(d);
-			return (NULL);
-		}
-		for (i = 0; i <= len_owner; i++)
-			d->owner[i] = owner[i];
-	}
-	else
-		d->owner = NULL;
-
+	d->name = _strdup_custom(name);
+	d->owner = _strdup_custom(owner);
 	d->age = age;
+
+	if ((name != NULL && d->name == NULL) ||
+	    (owner != NULL && d->owner == NULL))
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+		return (NULL);
+	}
 
 	return (d);
 }
