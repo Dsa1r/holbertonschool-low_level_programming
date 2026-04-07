4c7a04a25c2f93639ea92f7d22f8c3f14d745dfa
sasa <dsds>
2026-04-07 14:43:41 +0000

    dds

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /*
  * int_index - kdkdkd
@@ -11,7 +12,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (size <= 0 || size == NULL)
+		return -1;
+	if (cmp == NULL)
 		return -1;
 
 	for (i = 0 ; i < size ; i++)
