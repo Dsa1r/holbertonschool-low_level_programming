ff0e054120084a678a4e5dabfa15887897df739c
sasa <dsds>
2026-04-07 14:05:07 +0000

    dddd

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,5 +1,12 @@
 #include "function_pointers.h"
 
+/**
+ * array_iterator - kjkjj
+ * @array: kkkk
+ * @size: jkjj
+ * @action: kdkdkkd
+ */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
