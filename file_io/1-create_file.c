36177c07e36eaef1e2173fff72c3b89c7cd5490b
ffffff <lllll>
2026-04-25 20:52:33 +0000

    ddd

---
+++ file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * create_file - creates a file and writes text into it
+ * @filename: name of the file
+ * @text_content: string to write
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+
+		i = write(fd, text_content, len);
+		if (i == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
